d0ac33755a82a349832b6b3594a62ef6ecf939ad
Paschalis Tsilias <p.tsilias@thebeat.co>
2021-01-11 21:23:16 +0200

    Fix tests to use external API only

--- constraints_test.go
+++ constraints_test.go
@@ -1,7 +1,9 @@
-package constraints
+package constraints_test
 
 import (
 	"testing"
+
+	c "github.com/tpaschalis/go-benchmark-constraints"
 )
 
 var benchfunc = func(b *testing.B) {
@@ -12,70 +14,70 @@ var benchfunc = func(b *testing.B) {
 }
 
 func TestMaxAllocations(t *testing.T) {
-	br1 := NewBR().Append(benchfunc, WithMaxAllocs(0), WithVerbose())
+	br1 := c.NewBR().Append(benchfunc, c.WithMaxAllocs(0), c.WithVerbose())
 	_, err1 := br1.Run()
 	if err1 == nil {
-		t.Errorf("Expected to fail WithMaxAllocs(0)")
+		t.Errorf("Expected to fail c.WithMaxAllocs(0)")
 	}
 
-	br2 := NewBR().Append(benchfunc, WithMaxAllocs(5), WithVerbose())
+	br2 := c.NewBR().Append(benchfunc, c.WithMaxAllocs(5), c.WithVerbose())
 	_, err2 := br2.Run()
 	if err2 != nil {
-		t.Errorf("Not expected to fail WithMaxAllocs(5)")
+		t.Errorf("Not expected to fail c.WithMaxAllocs(5)")
 	}
 }
 
 func TestMaxNsPerOp(t *testing.T) {
-	br1 := NewBR().Append(benchfunc, WithMaxNsPerOp(5), WithVerbose())
+	br1 := c.NewBR().Append(benchfunc, c.WithMaxNsPerOp(5), c.WithVerbose())
 	_, err1 := br1.Run()
 	if err1 == nil {
-		t.Errorf("Expected to fail WithMaxNsPerOp(5)")
+		t.Errorf("Expected to fail c.WithMaxNsPerOp(5)")
 	}
 
-	br2 := NewBR().Append(benchfunc, WithMaxNsPerOp(1_000_000), WithVerbose())
+	br2 := c.NewBR().Append(benchfunc, c.WithMaxNsPerOp(1_000_000), c.WithVerbose())
 	_, err2 := br2.Run()
 	if err2 != nil {
-		t.Errorf("Not expected to fail WithMaxNsPerOp(1_000_000)")
+		t.Errorf("Not expected to fail c.WithMaxNsPerOp(1_000_000)")
 	}
 }
 
 func TestMaxAllocatedBytes(t *testing.T) {
-	br1 := NewBR().Append(benchfunc, WithMaxAllocatedBytes(1), WithVerbose())
+	br1 := c.NewBR().Append(benchfunc, c.WithMaxAllocatedBytes(1), c.WithVerbose())
 	_, err1 := br1.Run()
 	if err1 == nil {
-		t.Errorf("Expected to fail WithMaxAllocatedBytes(1)")
+		t.Errorf("Expected to fail c.WithMaxAllocatedBytes(1)")
 	}
 
-	br2 := NewBR().Append(benchfunc, WithMaxAllocatedBytes(5000), WithVerbose())
+	br2 := c.NewBR().Append(benchfunc, c.WithMaxAllocatedBytes(5000), c.WithVerbose())
 	_, err2 := br2.Run()
 	if err2 != nil {
-		t.Errorf("Not expected to fail WithMaxAllocatedBytes(5000)")
+		t.Errorf("Not expected to fail c.WithMaxAllocatedBytes(5000)")
 	}
 }
 
 func TestMinMaxMBPerSec(t *testing.T) {
-	br1 := NewBR().Append(benchfunc, WithMaxMBPerSec(1), WithVerbose())
+	br1 := c.NewBR().Append(benchfunc, c.WithMaxMBPerSec(1), c.WithVerbose())
 	_, err1 := br1.Run()
 	if err1 == nil {
-		t.Errorf("Expected to fail WithMaxMBPerSec(1)")
+		t.Errorf("Expected to fail c.WithMaxMBPerSec(1)")
 	}
 
-	br2 := NewBR().Append(benchfunc, WithMaxMBPerSec(1_000_000), WithVerbose())
+	br2 := c.NewBR().Append(benchfunc, c.WithMaxMBPerSec(1_000_000), c.WithVerbose())
 	_, err2 := br2.Run()
 	if err2 != nil {
-		t.Errorf("Not expected to fail WithMaxMBPerSec(1_000_000)")
+		t.Errorf("Not expected to fail c.WithMaxMBPerSec(1_000_000)")
 	}
 
-	br3 := NewBR().Append(benchfunc, WithMinMBPerSec(1_000_000), WithVerbose())
+	br3 := c.NewBR().Append(benchfunc, c.WithMinMBPerSec(1_000_000), c.WithVerbose())
 	_, err3 := br3.Run()
 	if err3 == nil {
-		t.Errorf("Expected to fail WithMinMBPerSec(1_000_000)")
+		t.Errorf("Expected to fail c.WithMinMBPerSec(1_000_000)")
 	}
 
-	br4 := NewBR().Append(benchfunc, WithMinMBPerSec(1), WithVerbose())
+	br4 := c.NewBR().Append(benchfunc, c.WithMinMBPerSec(1), c.WithVerbose())
 	_, err4 := br4.Run()
 	if err4 != nil {
-		t.Errorf("Not expected to fail WithMinMBPerSec(1)")
+		t.Errorf("Not expected to fail c.WithMinMBPerSec(1)")
 	}
 }
 
